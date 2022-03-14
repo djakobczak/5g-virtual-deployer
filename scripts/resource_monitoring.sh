52464671e5bd0d479c9de70d4aa9f3817ede7638
Dawid Jakóbczak <dawidjakobczakbn@gmail.com>
2022-03-14 21:05:22 +0000

    tests: add connect ue test

--- scripts/resource_monitoring.sh
+++ scripts/resource_monitoring.sh
@@ -5,13 +5,15 @@ ACTION="${1:-"start"}"
 # use virt-top for cpu usage
 TEST_UNIQSUFIX="$(date +%H-%M-%S-%6N)"
 
-CPU_USAGE_UPF_FILENAME="cpu_upf_${TEST_UNIQSUFIX}.log"
-CPU_USAGE_CPLANE_FILENAME="cplane_upf_${TEST_UNIQSUFIX}.log"
-CPU_USAGE_HOST_FILENAME="host_virt_${TEST_UNIQSUFIX}.csv"
+CPU_USAGE_UPF_FILENAME="${2:-"/home/ops/monitoring/cpu_upf_${TEST_UNIQSUFIX}.log"}"
+CPU_USAGE_CPLANE_FILENAME="${3:-"/home/ops/monitoring/cplane_upf_${TEST_UNIQSUFIX}.log"}"
+CPU_USAGE_HOST_FILENAME="${4:-"host_virt_${TEST_UNIQSUFIX}.csv"}"
 
 if [[ "${ACTION}" == "start" ]]; then
     # start cpu monitoring on vms
+    ssh ops@192.168.122.10 "mkdir -p $(dirname ${CPU_USAGE_CPLANE_FILENAME})"
     ssh ops@192.168.122.10 "nohup sudo bash scripts/log_server_cpu_usage.sh ${CPU_USAGE_CPLANE_FILENAME} &> ${CPU_USAGE_CPLANE_FILENAME} &"
+    ssh ops@192.168.122.100 "mkdir -p $(dirname ${CPU_USAGE_UPF_FILENAME})"
     ssh ops@192.168.122.100 "nohup sudo bash scripts/log_server_cpu_usage.sh ${CPU_USAGE_UPF_FILENAME} &> ${CPU_USAGE_UPF_FILENAME} &"
     echo "ssh"
     # start host monitoring
--- scripts/test_connect_ues.sh
+++ scripts/test_connect_ues.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
 
-N_UES="${1:-"10"}"  # number of ues start in one iteration
+N_UES="${1:-"20"}"  # number of ues start in one iteration
 N_ITERATIONS="${2:-"10"}"
+LOGS_PATH="${3:-"/home/djak/5gcore_measurements/vms/test-connect-ues"}"
+UNIQ_TEST_NAME="test-$(date +"%H-%M-%S-%6N")"
+LOGS_FULL_PATH="${LOGS_PATH}/${UNIQ_TEST_NAME}"
 
-./resource_monitoring.sh start
+CONNECT_UES_SCRIPT="/home/ops/scripts/connect_ues.sh"
+
+mkdir -p ${LOGS_FULL_PATH} || exit 1
+
+TEST_UNIQSUFIX="$(date +%H-%M-%S-%6N)"
+CPU_USAGE_UPF_FILENAME="/home/ops/monitoring/cpu_upf_${TEST_UNIQSUFIX}.log"
+CPU_USAGE_CPLANE_FILENAME="/home/ops/monitoring/cplane_upf_${TEST_UNIQSUFIX}.log"
+CPU_USAGE_HOST_FILENAME="host_virt_${TEST_UNIQSUFIX}.csv"
+
+./resource_monitoring.sh start ${CPU_USAGE_UPF_FILENAME} ${CPU_USAGE_CPLANE_FILENAME} ${CPU_USAGE_HOST_FILENAME}
 
 # connect to ue and run bootstrap script
-ssh ops@192.168.122.60 "sudo connect_ues.sh ${N_UES} ${N_ITERATIONS}"
+ssh ops@192.168.122.60 "sudo bash ${CONNECT_UES_SCRIPT} ${N_UES} ${N_ITERATIONS}"
 
 # gather logs
+scp ops@192.168.122.10:${CPU_USAGE_CPLANE_FILENAME} ${LOGS_FULL_PATH}/cplane_cpu.log
+scp ops@192.168.122.100:${CPU_USAGE_UPF_FILENAME} ${LOGS_FULL_PATH}/upf_cpu.log
+mv ${CPU_USAGE_HOST_FILENAME} ${LOGS_FULL_PATH}/
 
 # stop monitoring
 ./resource_monitoring.sh stop
